d41dfaa6726fab8f87d34643c7d0cd993a871fdc
caohengqiag <1278833522@qq.com>
2013-05-20 09:32:45 +0800

    1

--- jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/viewer/HTMLGraphicalViewer.java
+++ jsf/plugins/org.eclipse.jst.pagedesigner/src/org/eclipse/jst/pagedesigner/viewer/HTMLGraphicalViewer.java
@@ -37,7 +37,9 @@ import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
 import org.w3c.dom.Node;
 
 import com.founder.fix.studio.platformdesigner.views.BizObjFieldsView;
+import com.founder.fix.studio.wpeformdesigner.jst.pagedesigner.properties.FixPropertySource;
 import com.founder.fix.studio.wpeformdesigner.jst.pagedesigner.properties.ModelCommet;
+import com.founder.fix.studio.wpeformdesigner.jst.pagedesigner.properties.FixPropertySourceProvider;
 
 /**
  * For the GraphicalViewer selection management, we have two different selection
@@ -357,7 +359,14 @@ public class HTMLGraphicalViewer extends ScrollingGraphicalViewer implements
 		 *	@author Fifteenth
 		 *		select field
 		 */
-		Node commentNode = ModelCommet.getNode((ElementStyleImpl)((ElementEditPart)editpart).getDOMNode(), ""); //$NON-NLS-1$
+		ElementStyleImpl impl = (ElementStyleImpl)((ElementEditPart)editpart).getDOMNode();
+		Node commentNode = ModelCommet.getNode(impl, ""); //$NON-NLS-1$
+		
+		if(!FixPropertySource.isInit){
+			FixPropertySourceProvider provider = new FixPropertySourceProvider(""); //$NON-NLS-1$
+			FixPropertySource fixPropertySource = (FixPropertySource) provider.getPropertySource(impl);
+			fixPropertySource.getPropertyDescriptors();
+		}
 		
 		String selectField = BizObjFieldsView.getSelectionField(commentNode);
 		
